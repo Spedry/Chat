4bcd78019a176723c566873e452bdc3ca9a04e78
Spedry <spedrysk@gmail.com>
2020-08-13 17:09:32 +0200

    Vytvorenie KeyNote file

---
+++ src/sk/Spedry/gui/test.java
@@ -0,0 +1,33 @@
+package sk.Spedry.gui;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class test {
+    public static void main(String[] args) {
+        while(true) {
+            try {
+                Socket socket = new Socket("HERNA-MASINERIA", 50000);
+
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+                JSONObject jsonObject = new JSONObject("{\"Data\":{\"Username\":\"meno\",\"Password\":\"heslo\"},\"ID\":\"LoU\"}");
+                out.println(jsonObject.toString());
+                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                System.out.println("L");
+                System.out.println(in.readLine());
+                socket.close();
+            } catch (IOException | JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/sk/Spedry/server/test2.java
@@ -0,0 +1,42 @@
+package sk.Spedry.server;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import sk.Spedry.Client.Client;
+import sk.Spedry.gui.App;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class test2 {
+    public static Client client;
+    public static Socket socket;
+    public static PrintWriter out;
+    public static BufferedReader in;
+
+    public static void main(String[] args) throws JSONException {
+        //java.awt.EventQueue.invokeLater(() -> {
+            socket = Client.createSocket();
+
+            try {
+                //socket.setKeepAlive(true);
+                out = new PrintWriter(socket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        //});
+
+        for (int i = 0; i < 10; i++) {
+            JSONObject jsonObject = new JSONObject("{\"Data\":{\"Username\":\"meno " + i + " \",\"Password\":\"heslo " + i + "\"},\"ID\":\"LoU\"}");
+            out.println(jsonObject.toString());
+        }
+
+
+
+    }
+}

09459dfa5707b4a954409b62b0723b662a4e5717
Spedry <spedrysk@gmail.com>
2021-02-18 15:43:11 +0100

    komentáre

--- Server/src/main/java/hash/Hashing.java
+++ Server/src/main/java/hash/Hashing.java
@@ -2,14 +2,11 @@ package hash;
 
 import lombok.Getter;
 import lombok.NonNull;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-import java.nio.charset.StandardCharsets;
 import java.security.*;
 
 public class Hashing {
-    private final Logger logger = LogManager.getLogger(this.getClass());
+    // Logger pri hashovaní nepoužívam z logického dôvodu
+    // komenty ako náhrada
     @Getter
     private byte[] salt;
 
@@ -17,19 +14,20 @@ public class Hashing {
         String generatedPassword = null;
         try {
             // Create MessageDigest instance for MD5
-            MessageDigest md = MessageDigest.getInstance("MD5");
-            //Add password bytes to digest
-            this.salt = generateSalt();
-            md.update(this.salt);
-            //Get the hash's bytes
+            final MessageDigest md = MessageDigest.getInstance("MD5");
+            // Add password bytes to digest
+            salt = generateSalt();
+            // Hashing
+            md.update(salt); // Combinations of passToHash and salt
+            // Get the hash's bytes
             byte[] bytes = md.digest(password.getBytes());
-            //This bytes[] has bytes in decimal format;
-            //Convert it to hexadecimal format
+            // This bytes[] has bytes in decimal format;
+            // Convert it to hexadecimal format
             StringBuilder sb = new StringBuilder();
-            for (byte aByte : bytes) {
-                sb.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
+            for (byte b : bytes) {
+                sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
             }
-            //Get complete hashed password in hex format
+            // Get complete hashed password in hex format
             generatedPassword = sb.toString();
         }
         catch (NoSuchAlgorithmException e) {
@@ -39,14 +37,14 @@ public class Hashing {
     }
 
     public byte[] generateSalt() throws NoSuchAlgorithmException {
-        //SHA1PRNG algorithm is used as cryptographically strong pseudo-random number generator based on the SHA-1
-        //Always use a SecureRandom generator
+        // SHA1PRNG algorithm is used as cryptographically strong pseudo-random number generator based on the SHA-1
+        // Always use a SecureRandom generator
         SecureRandom sr = SecureRandom.getInstance("SHA1PRNG");
-        //Create array for salt
+        // Create array for salt
         byte[] salt = new byte[16];
-        //Get a random salt
+        // Get a random salt
         sr.nextBytes(salt);
-        //return salt
+        // return salt
         return salt;
     }
 }

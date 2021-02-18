3c65392d39d3f0793655b7f82611bcfcc4425ebf
Spedry <spedrysk@gmail.com>
2021-02-18 15:53:58 +0100

    rozpracovaný popup

---
+++ Client/src/main/java/controllers/PopupController.java
@@ -0,0 +1,14 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class PopupController {
+    public AnchorPane anchorPopup;
+
+    public void loginIntoExistingRoomOnAction(ActionEvent actionEvent) {
+    }
+
+    public void createNewRoomOnAction(ActionEvent actionEvent) {
+    }
+}

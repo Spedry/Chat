93c94333e0356a48bb4f7b6c5bc371b523eb0fb9
Spedry <spedrysk@gmail.com>
2021-02-18 16:08:55 +0100

    odstránenie nepotrebných komentárov a funkcií opravenie metody stop()

---
+++ Client/src/main/java/client/Application.java
@@ -0,0 +1,44 @@
+package client;
+
+import controllers.ChatController;
+import controllers.LoginController;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.io.IOException;
+
+public class Application extends javafx.application.Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private static Stage window;
+    private static final Logger logger = LogManager.getLogger(Application.class);
+    LoginController loginController;
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        logger.info("App is starting...");
+        window = primaryStage;
+        loginController = new LoginController(window);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/loginScene.fxml"));
+        loader.setController(loginController);
+        Parent root = loader.load();
+        Scene login = new Scene(root);
+        window.setScene(login);
+        logger.info("Set scene to loginScene");
+        window.show();
+        logger.info("Show window of app");
+    }
+
+    @Override
+    public void stop() {
+        try {
+            loginController.getClientSide().closeSocket();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}

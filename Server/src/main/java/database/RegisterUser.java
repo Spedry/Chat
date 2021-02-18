c7096cf22b266854acbb2ccd5c2c6bd1b2f8b44b
Spedry <spedrysk@gmail.com>
2021-02-18 15:46:35 +0100

    upravenie spôsobu napájania sa na DB

--- Server/src/main/java/database/RegisterUser.java
+++ Server/src/main/java/database/RegisterUser.java
@@ -1,15 +1,11 @@
 package database;
 
-import lombok.Getter;
 import lombok.NonNull;
-import lombok.Setter;
 import org.apache.logging.log4j.LogManager;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.apache.logging.log4j.Logger;
-
 import java.sql.*;
-import java.util.Arrays;
 
 public class RegisterUser extends AbsUser {
     private final Logger logger = LogManager.getLogger(this.getClass());
@@ -21,50 +17,47 @@ public class RegisterUser extends AbsUser {
 
     public boolean Register() {
         logger.info("Start of attempt to register");
-        boolean registered = false;
-        String jdbcURL = "jdbc:mysql://localhost:3306/chat?useSSL=false&allowPublicKeyRetrieval=true";
         logger.info("Connecting to database");
-        try (
-                Connection conn = DriverManager.getConnection(jdbcURL, "root", "admin");
-                Statement stmt = conn.createStatement()
-        ) {
+        boolean registered = false;
+        try (Connection conn = DriverManager.getConnection(getJdbcURL(), getUser(), getPassword())) {
             logger.info("Successfully connected to database");
-            logger.info("SELECT");
+            logger.info("Creating SELECT");
             String SELECT = "SELECT UserName FROM Users";
             logger.debug("SELECT is: " + SELECT);
-            ResultSet QUERY = stmt.executeQuery(SELECT);
             boolean possibleToReg = true;
-            logger.info("Querying trough database");
-            while(QUERY.next()) {
-                logger.info("TEST");
-                String UserName = QUERY.getString("UserName");
-                if (UserName.equals(getUserName())) {
-                    possibleToReg = false;
-                    logger.info("The inserted name is already in use");
-                    break;
+            try (PreparedStatement preparedStmt = conn.prepareStatement(SELECT)) {
+                ResultSet QUERY = preparedStmt.executeQuery(SELECT);
+                logger.info("Querying trough database");
+                                while(QUERY.next()) {
+                    String UserName = QUERY.getString("UserName");
+                    if (UserName.equals(getUserName())) {
+                        possibleToReg = false;
+                        logger.info("The inserted name is already in use");
+                        break;
+                    }
                 }
+            }  catch (JSONException jsone) {
+                logger.error("Error with JSONObject", jsone);
             }
-            if (possibleToReg) {
-                logger.info("Second hash");
-                secondHash();
-                logger.info("SELECT");
-                SELECT = " INSERT INTO users (UserName, UserHash, PublicKey, PrivateKey)"
-                        + " VALUES (?, ?, ?, ?)";
-                try (PreparedStatement preparedStmt = conn.prepareStatement(SELECT)) {
+            logger.info("Creating SELECT");
+            SELECT = "INSERT INTO users (UserName, UserHash, PublicKey, PrivateKey) VALUES (?, ?, ?, ?)";
+            try (PreparedStatement preparedStmt = conn.prepareStatement(SELECT)) {
+                if (possibleToReg) {
+                    logger.info("Second hash");
+                    secondHash();
+                    logger.debug("SELECT is: " + SELECT);
+                    logger.info(getUserName() + getUserHash());
                     preparedStmt.setString(1, getUserName());
                     preparedStmt.setString(2, getUserHash());
                     preparedStmt.setBytes(3, getPublicKey());
                     preparedStmt.setBytes(4, getPrivateKey());
-                    logger.info("Select: " + SELECT); //(\"" + getStringfromJson(user_name) + "\",\"" + getStringfromJson(hash) + "\")"
                     preparedStmt.execute();
                     registered = true;
                     logger.info("New user successfully registered");
                 }
             }
-        } catch (SQLException sqle) {
+        }catch (SQLException sqle) {
             logger.error("Error trying connect to database", sqle);
-        } catch (JSONException jsone) {
-            logger.error("Error with JSONObject", jsone);
         }
         return registered;
     }

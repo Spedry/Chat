ee81bd5849e72b3b10043061c0e439e6fff1c5a8
Spedry <spedrysk@gmail.com>
2021-03-12 16:35:14 +0100

    zmena názvu

--- Server/src/main/java/database/LoginToMySQL.java
+++ Server/src/main/java/database/LoginToMySQL.java
@@ -2,7 +2,7 @@ package database;
 
 import lombok.Getter;
 
-public abstract class loginToMySQL {
+public abstract class LoginToMySQL {
     @Getter
     private final String jdbcURL = "jdbc:mysql://localhost:3306/chat?useSSL=false&allowPublicKeyRetrieval=true",
             user = "root",

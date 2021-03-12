309d9562bb48ee4d9a6096bb6e41f17157daa105
Spedry <spedrysk@gmail.com>
2021-03-12 16:48:23 +0100

    vytvorenie popup scény a načítanie exitujúcej miestnosti

--- Client/src/main/java/controllers/list/RoomCell.java
+++ Client/src/main/java/controllers/list/RoomCell.java
@@ -2,23 +2,20 @@ package controllers.list;
 
 import client.Application;
 import client.MessageSender;
+import controllers.ChatController;
 import controllers.PopupController;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
-import javafx.stage.Popup;
 import javafx.stage.Stage;
-import lombok.NonNull;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.json.JSONException;
-import org.json.JSONObject;
+import shortcuts_for_M_and_V.Methods;
+import shortcuts_for_M_and_V.Variables;
 
 import java.io.IOException;
 
@@ -26,39 +23,49 @@ public class RoomCell extends ListCell<Room> {
     private final Logger logger = LogManager.getLogger(this.getClass());
     private final Button openRoom = new Button("OPEN");
     private final Button createRoom = new Button("CREATE");
-    private final Stage window;
+    private final ChatController chatController;
     private final MessageSender messageSender;
-    public RoomCell(MessageSender messageSender, Stage window) {
+
+    public RoomCell(MessageSender messageSender, Stage window, ChatController chatController) {
         super();
         this.messageSender = messageSender;
-        this.window = window;
+        this.chatController = chatController;
         //button.setOnAction(event -> getListView().getItems().remove(getItem())); mazanie do budúcna to bude užitočné
         openRoom.setOnAction(e -> {
             logger.info("I'll load an existing room");
             loadAnExistingRoom();
+            chatController.clearMessageListView();
         });
         createRoom.setOnAction(e -> {
             logger.info("I'll create a new room");
-
-            AnchorPane anchorPane;
-            Popup popup = new Popup();
-            FXMLLoader loader = new FXMLLoader(Application.class.getResource("/fxml/popup.fxml"));
-
-
-
-            final Stage dialog = new Stage();
-            dialog.initModality(Modality.APPLICATION_MODAL);
-            dialog.initOwner(window);
+            Stage popup = new Stage();
+            popup.initModality(Modality.APPLICATION_MODAL);
+            popup.initOwner(window);
             VBox dialogVbox = new VBox(20);
-            dialogVbox.getChildren().add(new Text("This is a Dialog"));
-            Scene dialogScene = new Scene(dialogVbox, 300, 200);
-            dialog.setScene(dialogScene);
-            dialog.show();
+            dialogVbox.getChildren().add(new Text("This is Popup"));
+            Scene newScene = new Scene(dialogVbox, 300, 200);
+            FXMLLoader loader = new FXMLLoader(Application.class.getResource("/fxml/popup.fxml"));
+            PopupController popupController = new PopupController(popup, messageSender);
+
+            loader.setController(popupController);
+            try {
+                newScene = new Scene(loader.load());
+            } catch (IOException ioException) {
+                ioException.printStackTrace();
+            }
+            popup.setScene(newScene);
+            popup.show();
         });
     }
 
+
     private void loadAnExistingRoom() {
-        messageSender.printWriter(createJson("LR", "RoomID", getItem().getID(), null, null));
+        logger.info("Sending a request to load an existing room");
+        messageSender.printWriter(Methods.createJson(Variables.LOAD_AN_EXISTING_ROOM, Variables.ROOM_ID, getItem().getID()));
+        logger.info("Setting current room ID to: " + getItem().getID());
+        chatController.setCurrentRoomID(getItem().getID());
+        logger.info("Current room ID was set");
+        chatController.messageField.clear();
     }
 
     @Override
@@ -66,27 +73,12 @@ public class RoomCell extends ListCell<Room> {
         super.updateItem(room, empty);
         setGraphic(null);
 
-        if (room != null && !empty && room.getID() != 000) {
+        if (room != null && !empty && room.getID() != 0) {
             openRoom.setText(room.getName());
             setGraphic(openRoom);
-        } else if (room != null && !empty && room.getID() == 000) {
+        } else if (room != null && !empty && room.getID() == 0) {
             openRoom.setText(room.getName());
             setGraphic(createRoom);
         }
-    }
-
-    private JSONObject createJson(@NonNull String IDString,
-                                  @NonNull String dataOne,
-                                  @NonNull Object objectOne,
-                                  String dataTwo,
-                                  Object objectTwo) throws JSONException {
-        String data = "Data";
-        JSONObject dataOfJsonObject = new JSONObject()
-                .put(dataOne, objectOne);
-        if (dataTwo != null && objectTwo != null)
-            dataOfJsonObject.put(dataTwo, objectTwo);
-        return new JSONObject()
-                .put("ID", IDString)
-                .put(data, dataOfJsonObject);
     }
 }
